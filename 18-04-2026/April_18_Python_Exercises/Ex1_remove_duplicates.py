601aca6520073898ff50e8428de7e27ea8f27a9c
Dharshini Babu <155763250+buddingengineer02@users.noreply.github.com>
2026-04-18 12:08:51 +0530

    Delete 18-04-2026/April_18_Python_Exercises directory

--- 18-04-2026/April_18_Python_Exercises/Ex1_remove_duplicates.py
+++
@@ -1,8 +0,0 @@
-# Exercise 1 – Remove Duplicates from a List
-customers = [101, 102, 103, 101, 104, 102, 105]
-#1. Remove duplicate IDs
-unique_customers = list(set(customers))
-#2. Print unique customers
-print("Unique customers:", unique_customers)
-#3. Print total number of unique customers
-print("Total unique customers:", len(unique_customers))
--- 18-04-2026/April_18_Python_Exercises/Ex6_common_students.py
+++
@@ -1,9 +0,0 @@
-# Exercise 6 – Common Students in Two Classes
-classA = {"Rahul", "Sneha", "Amit", "Neha"}
-classB = {"Sneha", "Amit", "Karan", "Riya"}
-#1. Students in both classes
-print("Students in both classes:", classA.intersection(classB))
-#2. Students only in Class A
-print("Students only in Class A:", classA.difference(classB))
-#3. All unique students
-print("All unique students:", classA.union(classB))
